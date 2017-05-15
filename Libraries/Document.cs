ceb3b82fc1c809e1c7597caf57bc63ce925146cb
Cesare Caoduro <cesacaod@outlook.com>
2017-05-15 16:54:05 +0200

    Rebars tools added

--- Libraries/Document.cs
+++ Libraries/Document.cs
@@ -1,7 +1,11 @@
 ﻿using Autodesk.DesignScript.Runtime;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using CoreNodeModels;
+using Dynamo.Graph.Nodes;
+using Dynamo.Utilities;
 using DynaToolsFunctions;
+using ProtoCore.AST.AssociativeAST;
 using Revit.Elements;
 using RevitServices.Persistence;
 using System;
@@ -70,8 +74,8 @@ namespace DocumentTools
         /// <param name="detach"></param>
         /// <param name="audit"></param>
         /// <returns></returns>
-        [MultiReturn(new[] { "document" })]
-        public static Dictionary<string, object> createNavisViewBackground(Document document)
+        [MultiReturn(new[] { "document", "message" })]
+        public static Dictionary<string, object> createNavisView(Document document)
         {
             string message = "";
 
@@ -85,7 +89,7 @@ namespace DocumentTools
             //OpenOptions openOpt = new OpenOptions();
             try
             {
-                
+
                 //openedDoc = app.OpenDocumentFile(path, openOpt);
                 var direction = new XYZ(-1, 1, -1);
                 var collector = new FilteredElementCollector(document);
@@ -96,7 +100,6 @@ namespace DocumentTools
                 {
                     ttNew.Start();
                     View3D navisView = View3D.CreateIsometric(document, viewFamilyType.Id);
-
                     navisView.SetOrientation(new ViewOrientation3D(direction, new XYZ(0, 1, 1), new XYZ(0, 1, -1)));
                     navisView.ViewName = "Navisworks";
                     ttNew.Commit();
@@ -122,10 +125,10 @@ namespace DocumentTools
         /// </summary>
         /// <param name="filePath"></param>
         /// <returns></returns>
-        [MultiReturn(new[] { "document" })]
-        public static Dictionary<string, object> openDocumentBackground(string filePath = null)
+        [MultiReturn(new[] { "document", "message" })]
+        public static Dictionary<string, object> openDocumentBackground(string filePath = null, OpenOptions openOption = null)
         {
-
+            string message = "";
             Document doc = DocumentManager.Instance.CurrentDBDocument;
             Document openedDoc = null;
             UIApplication uiapp = DocumentManager.Instance.CurrentUIApplication;
@@ -133,18 +136,20 @@ namespace DocumentTools
             UIDocument uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument;
 
             ModelPath path = ModelPathUtils.ConvertUserVisiblePathToModelPath(filePath);
-            OpenOptions openOpt = new OpenOptions();
+            
             try
             {
-                openedDoc = app.OpenDocumentFile(path, openOpt);
+                openedDoc = app.OpenDocumentFile(path, openOption);
+                message = "Opened";
             }
             catch (Exception ex)
             {
-                string message = ex.Message;
+                message = ex.Message;
             }
             return new Dictionary<string, object>
             {
-                {"document", openedDoc}
+                {"document", openedDoc},
+                {"message", message }
             };
 
         }
@@ -171,7 +176,7 @@ namespace DocumentTools
             try
             {
                 document.Close(save);
-                message = "closed";
+                message = "Closed";
             }
             catch (Exception ex)
             {
@@ -179,10 +184,66 @@ namespace DocumentTools
             }
             return new Dictionary<string, object>
             {
-                {"message", document}
+                {"message", message}
+            };
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="audit"></param>
+        /// <param name="detachFromCentralOption">
+        /// 0 = ClearTransmittedSaveAsNewCentral,
+        /// 1 = DetachAndDiscardWorksets,
+        /// 2 = DetachAndPreserveWorksets,
+        /// 3 = DoNotDetach
+        /// </param>
+        /// <returns></returns>
+        [MultiReturn(new[] { "openOption"})]
+        public static Dictionary<string, object> openDocumentOption(bool audit = false, int detachFromCentralOption = 3)
+        {
+            string message = "";
+            OpenOptions openOpt = new OpenOptions();
+
+            //Document doc = DocumentManager.Instance.CurrentDBDocument;
+            //Document openedDoc = null;
+            //UIApplication uiapp = DocumentManager.Instance.CurrentUIApplication;
+            //Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
+            //UIDocument uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument;
+
+            //ModelPath path = ModelPathUtils.ConvertUserVisiblePathToModelPath(filePath);
+            try
+            {
+
+                openOpt.Audit = false;
+                WorksetConfiguration workConf = new WorksetConfiguration(WorksetConfigurationOption.OpenAllWorksets);
+                openOpt.SetOpenWorksetsConfiguration(workConf);
+                switch(detachFromCentralOption)
+                {
+                    case 0: openOpt.DetachFromCentralOption = DetachFromCentralOption.ClearTransmittedSaveAsNewCentral; break;
+                    case 1: openOpt.DetachFromCentralOption = DetachFromCentralOption.DetachAndDiscardWorksets; break;
+                    case 2: openOpt.DetachFromCentralOption = DetachFromCentralOption.DetachAndPreserveWorksets; break;
+                    case 3: openOpt.DetachFromCentralOption = DetachFromCentralOption.DoNotDetach; break;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            return new Dictionary<string, object>
+            {
+                {"openOption", openOpt },
             };
 
         }
 
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    
+
 }
---
+++ Libraries/Rebars.cs
@@ -0,0 +1,57 @@
+﻿using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using DynaToolsFunctions;
+using Revit.Elements;
+using RevitServices.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RebarTools
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Rebars
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="elList"></param>
+        /// <returns></returns>
+        [MultiReturn(new[] { "Elements"})]
+        public static Dictionary<string, object> getHostElement(List<Revit.Elements.Element> elements)
+        {
+          
+            Document doc = DocumentManager.Instance.CurrentDBDocument;
+            List<Revit.Elements.Element> elIdList = new List<Revit.Elements.Element>();
+            //UIApplication uiapp = DocumentManager.Instance.CurrentUIApplication;
+            //Autodesk.Revit.ApplicationServices.Application app = uiapp.Application;
+            //UIDocument uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument;
+
+            foreach (Revit.Elements.Element e in elements)
+            {
+                Autodesk.Revit.DB.Element el = doc.GetElement(e.UniqueId.ToString());
+                Rebar r = el as Rebar;
+                ElementId elId = r.GetHostId();
+                elIdList.Add(doc.GetElement(elId).ToDSType(true));
+            }
+
+            return new Dictionary<string, object>
+            {
+                { "hostElements", elIdList},
+            };
+
+        }
+
+        
+    }
+
+   
+
+   
+
+}
